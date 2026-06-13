e2eafe480de61f44abf44c44bad77fd25e926f9f
QingMo <QINGMOA5@gmail.com>
2026-06-13 10:47:30 -0400

    [+] 准星指向可拾取的地面物品时，使用 Minecraft 原版 entity outline 发光轮廓进行高亮

--- targets/forge-1.20.1/src/main/java/com/dreamingfish/gridinventory/target/forge1201/mixin/client/MinecraftMixin.java
+++ targets/forge-1.20.1/src/main/java/com/dreamingfish/gridinventory/target/forge1201/mixin/client/MinecraftMixin.java
@@ -1,16 +1,20 @@
 package com.dreamingfish.gridinventory.target.forge1201.mixin.client;
 
+import com.dreamingfish.gridinventory.client.pickup.ClientItemTargeting;
 import com.dreamingfish.gridinventory.common.network.OpenPlayerGridInventoryMessage;
 import com.dreamingfish.gridinventory.platform.GridInventoryServices;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.Options;
 import net.minecraft.client.multiplayer.MultiPlayerGameMode;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Minecraft.class)
 public abstract class MinecraftMixin {
@@ -42,6 +46,15 @@ public abstract class MinecraftMixin {
 
         while (options.keyInventory.consumeClick()) {
             GridInventoryServices.network().sendToServer(OpenPlayerGridInventoryMessage.INSTANCE);
+        }
+    }
+
+    @Inject(method = "shouldEntityAppearGlowing", at = @At("HEAD"), cancellable = true)
+    private void df_grid_inventory$highlightTargetedDroppedItem(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+        if (GridInventoryServices.clientConfig().highlightTargetItem()
+                && entity instanceof ItemEntity
+                && entity.getId() == ClientItemTargeting.currentTargetItemEntityId()) {
+            cir.setReturnValue(true);
         }
     }
 }
